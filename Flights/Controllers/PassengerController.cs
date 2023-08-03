40d291528bb7b0d59a707e54f5dd93ff8671011c
Jacob Sonn <jakesonn321@gmail.com>
2023-08-03 14:59:38 -0400

    Day 9

--- Flights/Controllers/PassengerController.cs
+++ Flights/Controllers/PassengerController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Flights.Dtos;
+using Flights.ReadModels;
 
 namespace Flights.Controllers
 {
@@ -12,14 +13,40 @@ namespace Flights.Controllers
     [ApiController]
     public class PassengerController : ControllerBase
     {
+        static private IList<NewPassengerDto> Passengers = new List<NewPassengerDto>();
+
         [HttpPost]
         [ProducesResponseType(201)]
         [ProducesResponseType(400)]
         [ProducesResponseType(500)]
         public IActionResult Register(NewPassengerDto dto)
         {
-            throw new NotImplementedException(); 
+            Passengers.Add(dto);
+            System.Diagnostics.Debug.WriteLine(Passengers.Count);
+            return CreatedAtAction(nameof(Find), new {email = dto.Email});
+            
         }
+        
+        [HttpGet("{email}")]
+        public ActionResult<PassengerRm> Find(string email)
+        {
+            var passenger = Passengers.FirstOrDefault(p => p.Email == email);
+
+            if (passenger == null)
+            {
+                return NotFound();
+            }
+
+            var rm = new PassengerRm(
+                passenger.Email,
+                passenger.FirstName,
+                passenger.LastName,
+                passenger.Gender
+                );
+                
+            return Ok(rm);
+        }
+
     }
 }
 
---
+++ Flights/ReadModels/PassengerRm.cs
@@ -0,0 +1,7 @@
+namespace Flights.ReadModels;
+
+public record PassengerRm(
+    string Email,
+    string FirstName,
+    string LastName,
+    bool Gender);
